cefecde07ee53f67f5d85155014fa72a0410874d
lkang172 <kangzundao@gmail.com>
2025-03-31 21:48:41 -0400

    create dynamic routes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,10 @@
 "use server";
 import { z } from "zod";
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
@@ -12,10 +17,18 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
-  };
-  console.log(rawFormData);
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
